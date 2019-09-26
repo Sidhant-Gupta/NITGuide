70a81e71b3852f737bc4fc97af8d63dcdcdff63a
Sejal-G <sejalcjm@gmail.com>
2019-09-26 10:49:56 +0530

    adding listeners in branch_paper class

--- app/src/main/java/com/example/nit_guide/branch_paper.java
+++ app/src/main/java/com/example/nit_guide/branch_paper.java
@@ -3,6 +3,8 @@ package com.example.nit_guide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class branch_paper extends AppCompatActivity {
 
@@ -10,5 +12,56 @@ public class branch_paper extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_branch_paper);
+        //startActivity();
+        ImageView comp=(ImageView)findViewById(R.id.computer_img);
+        ImageView it=(ImageView)findViewById(R.id.it_img);
+        ImageView civil=(ImageView)findViewById(R.id.civil_img);
+        ImageView ece=(ImageView)findViewById(R.id.ece_img);
+        ImageView elect=(ImageView)findViewById(R.id.electrical_img);
+        ImageView mech=(ImageView)findViewById(R.id.mechanical_img);
+
+        comp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        civil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        it.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        elect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        ece.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
+
 }
