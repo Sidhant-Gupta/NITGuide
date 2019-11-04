a5f239ed8e6a6dfc12769143a0be831235ec8e33
Sidhan-Gupta <guptasidhant04@gmail.com>
2019-11-04 19:07:58 +0530

    Contacts made Dynamic using realtime database.

--- app/src/main/java/com/example/nit_guide/contact.java
+++ app/src/main/java/com/example/nit_guide/contact.java
@@ -1,11 +1,13 @@
 package com.example.nit_guide;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +19,7 @@ public class contact extends AppCompatActivity {
     public void addItemondpSpinner(){
         dp_spin = (Spinner) findViewById(R.id.dp_spin);
         List<String> list = new ArrayList<String>();
+        list.add("Departments");
         list.add("CSE");
         list.add("ECE");
         list.add("IT");
@@ -28,6 +31,7 @@ public class contact extends AppCompatActivity {
     public void addItemonhostelSpinner(){
         hostel_spin=(Spinner) findViewById(R.id.hostel_spin);
         List<String> list = new ArrayList<String>();
+        list.add("Hostels");
         list.add("Boys hostel");
         list.add("Girls Hostel");
 //        list.add("IT");
@@ -41,6 +45,7 @@ public class contact extends AppCompatActivity {
     public void addItemonEmergencySpinner(){
         emergency_spin=(Spinner) findViewById(R.id.emergency_spin);
         List<String> list = new ArrayList<String>();
+        list.add("Emergency");
         list.add("Health Centre");
 //        list.add("IT");
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
@@ -58,5 +63,27 @@ public class contact extends AppCompatActivity {
         addItemondpSpinner();
         addItemonhostelSpinner();
         addItemonEmergencySpinner();
+
+        dp_spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String ii=Integer.toString(i);
+                if (i == 0) {
+                    return;
+                }
+                else {
+                    Intent mintent = new Intent(getApplicationContext(), contact_list.class);
+                    mintent.putExtra("select", ii);
+                    startActivity(mintent);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
