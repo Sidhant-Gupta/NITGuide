5162533b9794f998ec78a81c3fae79dcad3fbe99
Sidhan-Gupta <guptasidhant04@gmail.com>
2020-10-07 14:15:39 +0530

    minor bug fixed

--- app/src/main/java/com/example/nit_guide/activities/ListSubjects.java
+++ app/src/main/java/com/example/nit_guide/activities/ListSubjects.java
@@ -53,7 +53,7 @@ public class ListSubjects extends AppCompatActivity {
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem (tab.getPosition ( ));
 
-                if (tab.getPosition ( ) == 0 || tab.getPosition ( ) == 1 || tab.getPosition ( ) == 2) {
+                if (tab.getPosition ( ) == 0 || tab.getPosition ( ) == 1 || tab.getPosition ( ) == 2 || tab.getPosition()==3) {
                     pagerAdapter.notifyDataSetChanged ( );
                 }
 
